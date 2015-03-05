af01420128ccd5f01925c036fd668678135c8ab9
bshea5 <shea.goog.25@gmail.com>
2015-03-04 20:36:48 -0500

    event precursor
    just figuring out how I want to set up an event object.
    Event object can contain dialogs, scripts to run, and NPC's to generate.

---
+++ Event.cpp
@@ -0,0 +1,44 @@
+#include "Event.h"
+
+Event::Event(GameApplication* a)
+{
+
+}
+
+Event::Event(GameApplication* a, std::vector<std::string> sf)
+{
+
+}
+
+Event::~Event(void)
+{
+
+}
+
+// run the event!
+void
+Event::run()
+{
+
+}
+
+// run a single script file
+void 
+Event::runScript(std::string)
+{
+
+}
+
+// spawn an NPC in the level
+void
+Event::spawnNPC(NPC* npc)
+{
+
+}
+
+// print dialog to the dialog box
+void 
+Event::printDialog(std::string)
+{
+
+}
