9234d2a12456a3ab41ac07295592a26708afea5b
Brad <brad.anderson58@gmail.com>
2015-04-07 13:58:01 -0400

    RockWall Normal Mapping
    Make sure to put updated toon.material file in the
    Ogre/Media/Materials/Scripts directory
    Also, add wall_ps.glsl and wall_vs.glsl to the directory
    Ogre/Media/Materials/Programs/GLSL

---
+++ shaders/wall_ps.glsl
@@ -0,0 +1,119 @@
+/* -------------------------------------------------------
+
+This shader implements a spotlight per pixel using  the 
+diffuse, specular, and ambient terms acoording to "Mathematics of Lighthing" 
+as found in the book "OpenGL Programming Guide" (aka the Red Book)
+
+Antonio Ramires Fernandes
+
+--------------------------------------------------------- */
+
+uniform sampler2D color_tex;
+uniform sampler2D normal_tex;
+
+varying vec3 ambient, diffuse, specular;
+varying vec3 normal;
+varying vec2 texCoord;
+varying vec4 position;
+varying vec3 lightDir, eyeDir;
+
+
+//Auto magic tangent space http://www.geeks3d.com/20130122/normal-mapping-without-precomputed-tangent-space-vectors/
+mat3 calcTBN(vec3 pos, vec2 texcoord, vec3 normal) {
+	// compute derivations of the world position
+	vec3 p_dx = dFdx(pos);
+	vec3 p_dy = dFdy(pos);
+	// compute derivations of the texture coordinate
+	vec2 tc_dx = dFdx(texcoord);
+	vec2 tc_dy = dFdy(texcoord);
+	// compute initial tangent and bi-tangent
+	vec3 t = normalize( tc_dy.y * p_dx - tc_dx.y * p_dy );
+	vec3 b = normalize( tc_dy.x * p_dx - tc_dx.x * p_dy ); // sign inversion
+	// get new tangent from a given mesh normal
+	vec3 n = normalize(normal);
+	vec3 x = cross(n, t);
+	t = cross(x, n);
+	t = normalize(t);
+	// get updated bi-tangent
+	x = cross(b, n);
+	b = cross(n, x);
+	b = normalize(b);
+	return mat3(t, b, n);
+}
+
+
+void main()
+{
+	vec3 normSample = texture2D(normal_tex, texCoord).xyz*2.0-1.0;
+	normSample.xy *= 0.5;
+	mat3 tbn = calcTBN(position.xyz, texCoord, normalize(normal));
+	vec3 N = tbn * normSample;
+
+	vec3 L = lightDir;
+	vec3 E = eyeDir;
+	vec3 R = reflect(L, N);
+
+	float kD = max(0.0, dot(N, L));
+	float kS = pow(max(0.0, dot(R, E)), 16.0);
+	
+	vec4 diffSample = texture2D(color_tex, texCoord);
+	vec4 color = vec4(ambient + diffuse*kD*diffSample.rgb + specular*kS, diffSample.a);
+
+	gl_FragColor = vec4(color);
+/*
+	//assign texture color
+	vec4 texColor = texture2D(color_tex, texCoord);
+
+	//normal sample * tangent space equals new normal
+	vec3 normSamp = ( texture2D(normal_tex, texCoord).xyz * 2.0) - 1.0;
+	mat3 tangSpace = calcTBN(position.xyz, texCoord, normal);
+
+	vec3 norml = normalize(tangSpace * normSamp);
+
+	vec3 n,halfV;
+	float NdotL,NdotHV;
+	vec4 color = vec4(0); //ambientGlobal;
+	float att,spotEffect;
+	int get_light=0;
+
+	// a fragment shader can't write a verying variable, hence we need
+	// a new variable to store the normalized interpolated normal
+	n = normalize(norml);
+	
+	// compute the dot product between normal and ldir
+	NdotL = max(dot(n,normalize(lightDir)),0.0);
+
+
+	if (NdotL > 0.0) 
+	{
+		spotEffect = dot(normalize(gl_LightSource[0].spotDirection), normalize(-lightDir));
+		
+		float threshold = cos(gl_LightSource[0].spotCosCutoff); //spotCosCutoff is in radian
+
+		if (spotEffect >threshold ) 
+		{
+			spotEffect = pow(spotEffect, gl_LightSource[0].spotExponent);
+			att = spotEffect / (gl_LightSource[0].constantAttenuation +
+			                    gl_LightSource[0].linearAttenuation * dist +
+		                        gl_LightSource[0].quadraticAttenuation * dist * dist);
+
+
+			color += att * (diffuse * NdotL + ambient);
+
+			halfV = normalize(halfVector);
+			NdotHV = max(dot(n,halfV),0.0);
+			color += att * gl_FrontMaterial.specular * gl_LightSource[0].specular * pow(NdotHV,gl_FrontMaterial.shininess);
+			get_light=1;
+		}
+	}
+
+	if(get_light==0)
+	{		
+		att = 0.1 / (gl_LightSource[0].constantAttenuation +
+			         gl_LightSource[0].linearAttenuation * dist +
+		             gl_LightSource[0].quadraticAttenuation * dist * dist);		
+		color = att * ambient;
+	}
+
+	gl_FragColor = color * texColor + .05;*/
+}
---
+++ shaders/wall_vs.glsl
@@ -0,0 +1,75 @@
+/* -------------------------------------------------------
+
+This shader implements a spotlight per pixel using  the 
+diffuse, specular, and ambient terms acoording to "Mathematics of Lighthing" 
+as found in the book "OpenGL Programming Guide" (aka the Red Book)
+
+Antonio Ramires Fernandes
+
+--------------------------------------------------------- */
+
+varying vec3 ambient, diffuse, specular;
+varying vec3 normal;
+varying vec2 texCoord;
+varying vec4 position;
+varying vec3 lightDir, eyeDir;
+
+void main()
+{
+	texCoord = gl_MultiTexCoord0.xy;
+	position = gl_ModelViewMatrix * gl_Vertex;
+	normal = gl_NormalMatrix * gl_Normal;
+
+	lightDir = normalize((gl_ModelViewMatrix * vec4(gl_LightSource[0].position.xyz, 1.0)).xyz);
+	eyeDir = normalize(-position.xyz);
+
+	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+
+	ambient = vec3(0.1);//gl_FrontMaterial.ambient * gl_LightSource[0].ambient;
+	diffuse = vec3(0.8);//gl_FrontMaterial.diffuse * gl_LightSource[0].diffuse;
+	specular = vec3(0.5);
+
+/*
+	vec4 ecPos;
+	vec3 aux;
+	
+	//set the texCoord?
+	texCoord = gl_MultiTexCoord0.xy;
+
+	position = (gl_ModelViewMatrix * gl_Vertex);
+
+	// first transform the normal into eye space and normalize the result
+	normal = normalize(gl_NormalMatrix * gl_Normal);
+	
+	// now normalize the light's direction. Note that according to the
+	//OpenGL specification, the light is stored in eye space.
+	ecPos = gl_ModelViewMatrix * gl_Vertex;
+	aux = vec3(gl_LightSource[0].position-ecPos);
+	lightDir = normalize(aux);
+	
+	// compute the distance to the light source to a varying variable
+	dist = length(aux);
+
+	// Normalize the halfVector to pass it to the fragment shader
+		{
+		// compute position in camera space
+		//vec3 pos = vec3(ecPos);
+		
+		// compute eye vector and normalize it 
+		vec3 eye = normalize(-vec3(ecPos));
+
+		// compute the half vector
+		halfVector = normalize(lightDir + eye);
+	}
+
+	// Compute the diffuse, ambient and globalAmbient terms
+	diffuse = gl_FrontMaterial.diffuse * gl_LightSource[0].diffuse;
+	ambient = gl_FrontMaterial.ambient * gl_LightSource[0].ambient;
+	ambientGlobal = gl_LightModel.ambient * gl_FrontMaterial.ambient;
+	
+		
+	gl_Position = ftransform();
+
+*/
+
+} 
